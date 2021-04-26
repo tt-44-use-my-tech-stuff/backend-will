39d6d7febe0061517cd44269ac3919bdd910c0d7
Will B <wberman27@gmail.com>
2021-04-26 18:03:14 -0400

    it is reset. :)

--- api/data/seeds/02-roles.js
+++ api/data/seeds/02-roles.js
@@ -2,10 +2,6 @@
 exports.seed = function(knex){
     return knex('roles').insert([
         { role_name: "owner"},
-<<<<<<< HEAD
         { role_name: "renter"}
-=======
-        { role_name: "renteRRRRRRRRrRRRRRRrrr"}
->>>>>>> cae057e8567757efe9b428c8d1f0c563da0c63ac
     ])
 }
