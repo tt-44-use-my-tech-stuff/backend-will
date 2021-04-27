9e65db7b792d02fe0a4c53db7851eb3149657138
Will B <wberman27@gmail.com>
2021-04-27 13:31:48 -0400

    heroku push with role_id

--- api/data/seeds/03-users.js
+++ api/data/seeds/03-users.js
@@ -1,8 +1,8 @@
 
 exports.seed = function (knex){
     return knex('users').insert([
-        { username: "Will", password: "1234"},
-        { username: "Emma", password: "1234"},
-        { username: "Alexis", password: "1234"}
+        { username: "Will", password: "1234", role_id: 1},
+        { username: "Emma", password: "1234", role_id: 1},
+        { username: "Alexis", password: "1234", role_id: 1}
     ])
 }
